4f03e3143f740132158eb2e3bcb177ee6fb980ba
laxmangordon <52805467+laxmangordon@users.noreply.github.com>
2022-07-27 12:41:39 -0400

    Update sdCard.ino
    this adds the files menu to the selection screen

--- BoyancyEngineTest/sdCard.ino
+++ BoyancyEngineTest/sdCard.ino
@@ -2,25 +2,29 @@
 
 //CONSIDER using mavlink to be able to 'play back' logs on missioncontrol software
 // like this: https://github.com/mehdilauters/MavlinkSerialSdLogger
-
-
+// MAKE SURE THAT THE SD CARD LIBRARY IS THE NEWEST ONE, ONLY AVALIABLE WHEN DOWNLOADING IDE, NOT ON GITHUB
+File root;
+String userfirstinput;
+String usersecondinput;
 #define STORAGE_REFRESH_DELAY     1000
 
-const int chipSelect = 1; //SDCARD_SS_PIN;   //todo: remove board specific variables
+const int chipSelect = BUILTIN_SDCARD; //SDCARD_SS_PIN;   //todo: remove board specific variables
 //const int chipSelect = SDCARD_SS_PIN;   //todo: remove board specific variables
 
 bool validSDCard = false;
 long previousMillisStorage = 0;
 
 void setupStorage() {
-//  if (!SD.begin(chipSelect)) {
-//    validSDCard = false;
-//    Serial.println("NOT DETECTED: SD Card");
-//  }
-//  else {
-//    Serial.println("DETECTED: SD Card");
-//    validSDCard = true;
-//  }
+  if (!SD.begin(chipSelect)) {
+    validSDCard = false;
+    Serial.println("NOT DETECTED: SD Card");
+  }
+  else {
+    Serial.println("DETECTED: SD Card");
+    validSDCard = true;
+  }
+  root = SD.open("/");
+  Serial.println("type 'file' to go to files menu");
 }
 
 
@@ -42,17 +46,70 @@ void loopStorage() {
       dataString += ",";
       dataString += String(reservoirDistanceTOF);
 
+
+
       //File dataFile = SD.open("datalog.txt", FILE_WRITE);
-//      if (dataFile) {
-//        dataFile.println(dataString);
-//        dataFile.close();
-//        // print to the serial port too:
-//        Serial.println(dataString);
-//      }
-//      // if the file isn't open, pop up an error:
-//      else {
-//        Serial.println("SDCARD: error opening datalog.txt");
-//      }
+      //      if (dataFile) {
+      //        dataFile.println(dataString);
+      //        dataFile.close();
+      //        // print to the serial port too:
+      //        Serial.println(dataString);
+      //      }
+      //      // if the file isn't open, pop up an error:
+      //      else {
+      //        Serial.println("SDCARD: error opening datalog.txt");
+      //      }
     }
+  }
+}
+
+void loopStorageToSerial() {
+  if (Serial.available() > 0) {
+    userfirstinput = Serial.readStringUntil('\n');
+    Serial.print(userfirstinput);
+  }
+  if (userfirstinput == "ile") {// <-----This one right here officer
+    printDirectory(root, 0);
+    Serial.println("");
+    Serial.println("Which file would you like to open?");
+    for (;;) {
+      if (Serial.available() > 0) {
+        usersecondinput = Serial.readString();
+        char charBuf[50];
+        usersecondinput.toCharArray(charBuf, usersecondinput.length());
+
+        root = SD.open(charBuf);
+        Serial.println("");
+
+        while (root.available()) {
+          Serial.write(root.read());
+        }
+        root.close();
+      }
+    }
+  }
+}
+
+void printDirectory(File dir, int numTabs) {
+  while (true) {
+
+    File entry = dir.openNextFile();
+    if (! entry) {
+      // no more files
+      break;
+    }
+    for (uint8_t i = 0; i < numTabs; i++) {
+      Serial.print('\t');
+    }
+    Serial.print(entry.name());
+    if (entry.isDirectory()) {
+      Serial.println("/");
+      printDirectory(entry, numTabs + 1);
+    } else {
+      // files have sizes, directories do not
+      Serial.print("\t\t");
+      Serial.println(entry.size(), DEC);
+    }
+    entry.close();
   }
 }
